767ba4ad390b3b28fd15598c6e7f26d52d6b3fd9
Xuan Anh <xuananhngo301006@gmail.com>
2026-03-14 22:27:50 +0700

    Fix choice

--- src/Main.java
+++ src/Main.java
@@ -12,42 +12,44 @@ public class Main {
         DictionaryService service = new DictionaryService(repository);
         DictionaryController controller = new DictionaryController(service);
 
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
 
-        while (true) {
-
+        while (running) {
+            System.out.println();
             System.out.println("===== DICTIONARY =====");
             System.out.println("1. Add word");
             System.out.println("2. Search word");
             System.out.println("3. Exit");
+            System.out.print("Your choice:");
 
-            int choice = sc.nextInt();
-            sc.nextLine();
+            String choice = scanner.nextLine().trim();
+
 
             switch (choice) {
 
-                case 1:
-
+                case "1":
+                    {
                     System.out.print("Enter word: ");
-                    String word = sc.nextLine();
-
+                    String word = scanner.nextLine();
                     System.out.print("Enter meaning: ");
-                    String meaning = sc.nextLine();
-
+                    String meaning = scanner.nextLine();
                     controller.addWord(word, meaning);
                     break;
-
-                case 2:
-
+                }
+                case "2":
+                    {
                     System.out.print("Enter word: ");
-                    String search = sc.nextLine();
-
+                    String search = scanner.nextLine();
                     controller.searchWord(search);
                     break;
-
-                case 3:
-                    return;
-            }
+                }
+                case "3" : running = false; break;
+                default  : System.out.println("Invalid choice!"); break;    
+            }         
         }
+
+        System.out.println("Goodbye!");
+        scanner.close();
     }
 }
